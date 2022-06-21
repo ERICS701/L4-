a45b60d070aee5c5c52eee1e88109dcd9f19934c
Eric Sanchez <107879816+ERICS701@users.noreply.github.com>
2022-06-21 22:03:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+isHungry =input("Y/N are u Hungry ")
+isBored = input("Y/N are you Bored?")
+
+if (isHungry  == "Y" or isHungry == "Y"):
+  print("go eat")
+else:
+  print("Don't eat")
+if(isBored == "Y" or isBored =="Y"):
+ print (":go to sleep")
+else:
+  print("do nothing")
+
+userNumber = int(input("give me a Number:"))
+if(userNumber > 0):
+    print ("Your number is Positive")
+else:
+    print("Your number is Negative")
+
+#ask the user for their age. if the user is older then 17 , let them know that they can watch all movies
+#if they're younger than 13 , let them know that they can watch G-rated and PG-13
+#if they're younger than 13, they're only allowed to watch just G-rated movies.
+userAge = int("Enter your age:")
+
+if(userage >= 17):
+    print("you can watch all movies!")
+if(user age<17 and userAge >= 13)
+    print("you can watch G-Rated and PG-13 movies.")
+else:
+    print("you can only watch G-Rated movies.")
